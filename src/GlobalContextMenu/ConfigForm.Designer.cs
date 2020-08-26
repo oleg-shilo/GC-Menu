919933945a54a1b7358ffff977ec78e7401c0080
oleg-shilo <oleg.shilo@gmail.com>
2020-08-27 00:15:00 +1000

    - Added HotKey entry assistance in the ConfigForm

---
+++ src/GlobalContextMenu/ConfigForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace GlobalContextMenu
+{
+    partial class ConfigForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        void InitializeComponent()
+        {
+            this.OK = new System.Windows.Forms.Button();
+            this.cancel = new System.Windows.Forms.Button();
+            this.startWithWindows = new System.Windows.Forms.CheckBox();
+            this.hotKey = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // OK
+            // 
+            this.OK.Location = new System.Drawing.Point(254, 12);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(75, 23);
+            this.OK.TabIndex = 1;
+            this.OK.Text = "OK";
+            this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            // 
+            // cancel
+            // 
+            this.cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancel.Location = new System.Drawing.Point(254, 41);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(75, 23);
+            this.cancel.TabIndex = 2;
+            this.cancel.Text = "Cancel";
+            this.cancel.UseVisualStyleBackColor = true;
+            // 
+            // startWithWindows
+            // 
+            this.startWithWindows.AutoSize = true;
+            this.startWithWindows.Location = new System.Drawing.Point(12, 47);
+            this.startWithWindows.Name = "startWithWindows";
+            this.startWithWindows.Size = new System.Drawing.Size(117, 17);
+            this.startWithWindows.TabIndex = 1;
+            this.startWithWindows.Text = "Start with Windows";
+            this.startWithWindows.UseVisualStyleBackColor = true;
+            // 
+            // hotKey
+            // 
+            this.hotKey.Location = new System.Drawing.Point(77, 12);
+            this.hotKey.Name = "hotKey";
+            this.hotKey.Size = new System.Drawing.Size(156, 20);
+            this.hotKey.TabIndex = 0;
+            this.hotKey.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.hotKey_KeyPress);
+            this.hotKey.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.hotKey_PreviewKeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "HotKey:";
+            // 
+            // ConfigForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancel;
+            this.ClientSize = new System.Drawing.Size(343, 82);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.hotKey);
+            this.Controls.Add(this.startWithWindows);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.OK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "ConfigForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Settings";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        System.Windows.Forms.Button OK;
+        System.Windows.Forms.Button cancel;
+        System.Windows.Forms.CheckBox startWithWindows;
+        System.Windows.Forms.TextBox hotKey;
+        System.Windows.Forms.Label label1;
+    }
+}
---
+++ src/GlobalContextMenu/ConfigForm.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GlobalContextMenu
+{
+    public partial class ConfigForm : Form
+    {
+        public static bool PopUp()
+        {
+            bool result = false;
+            if (!showing)
+            {
+                showing = true;
+                result = new ConfigForm().ShowDialog() == DialogResult.OK;
+                showing = false;
+            }
+            return result;
+        }
+
+        string initialhotKey;
+        bool initialStartWithWindows;
+        static bool showing;
+
+        public ConfigForm()
+        {
+            InitializeComponent();
+            initialStartWithWindows = startWithWindows.Checked = Config.IsAppStartupConfigured();
+            initialhotKey = hotKey.Text = Config.ReadHotkey();
+            this.Load += ConfigForm_Load;
+        }
+
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+
+        private void ConfigForm_Load(object sender, EventArgs e)
+        {
+            SetForegroundWindow(this.Handle);
+        }
+
+        void OK_Click(object sender, EventArgs e)
+        {
+            if (initialhotKey != hotKey.Text)
+            {
+                Config.WriteHotkey(hotKey.Text);
+                DialogResult = DialogResult.OK;
+            }
+
+            if (initialStartWithWindows != startWithWindows.Checked)
+            {
+                Config.ConfigAppStartup(startWithWindows.Checked);
+                DialogResult = DialogResult.OK;
+            }
+        }
+
+        string hotKeyInput;
+
+        private void hotKey_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            var modifiers = "";
+
+            if (e.Modifiers.HasFlag(Keys.Control))
+                modifiers += "Ctrl+";
+
+            if (e.Modifiers.HasFlag(Keys.Shift))
+                modifiers += "Shift+";
+
+            if (e.Modifiers.HasFlag(Keys.Alt))
+                modifiers += "Alt+";
+
+            var key = e.KeyCode.ToString();
+
+            if (key.EndsWith("ShiftKey") || key.EndsWith("ControlKey") || key.EndsWith("Menu"))
+                hotKey.Text = "";
+            else
+                hotKey.Text = modifiers + key;
+        }
+
+        private void hotKey_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}
