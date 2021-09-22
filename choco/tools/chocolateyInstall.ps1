f164f307733f00f55c0ba6cecd6a4dcaf2527a5d
oleg.shilo <oleg.shilo@LBSMEL-1BQHTN2>
2021-09-22 22:04:34 +1000

    - Choco update

--- choco/tools/chocolateyInstall.ps1
+++ choco/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 $packageName = 'GC-Menu'
-$url = 'https://github.com/oleg-shilo/GC-Menu/releases/download/v1.0.1.0/gc-menu.7z'
+$url = 'https://github.com/oleg-shilo/GC-Menu/releases/download/v1.0.1/gc-menu.7z'
 
 Stop-Process -Name "gc-menu"
 
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$cheksum = '38A0EA67C1644076FB14810F098651E1572C1DE14BBD59F66FC9C8FFC4D19B0E'
+$cheksum = 'C0EA061AA2C749B1C7897CE447BBE0BFB8BB70F180F85146C17B780705696AD3'
 $checksumType = "sha256"
 
 # Download and unpack a zip file
