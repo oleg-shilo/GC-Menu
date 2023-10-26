e1517ac1497de4fb7cfe946d8ef027dd1588a9b8
oleg.shilo <Oleg.Shilo@LeicaBiosystems.com>
2023-10-26 21:54:41 +1100

    - v1.0.2.0 published on choco

--- choco/tools/chocolateyInstall.ps1
+++ choco/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 $packageName = 'GC-Menu'
-$url = 'https://github.com/oleg-shilo/GC-Menu/releases/download/v1.0.1/gc-menu.7z'
+$url = 'https://github.com/oleg-shilo/GC-Menu/releases/download/v1.0.2/gc-menu.7z'
 
 Stop-Process -Name "gc-menu"
 
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$cheksum = 'C0EA061AA2C749B1C7897CE447BBE0BFB8BB70F180F85146C17B780705696AD3'
+$cheksum = '9E686C66E9F4B40CC34315A2DE091F520C55BF7F5822461C11C7B9B35E459F08'
 $checksumType = "sha256"
 
 # Download and unpack a zip file
--- choco/update_package.cs
+++ choco/update_package.cs
@@ -1,16 +1,16 @@
 //css_dir %WINDOWS_DESKTOP_APP%
 //css_ac
-//css_inc %csscript_inc%\cmd.cs
+//css_inc cmd.cs
 using System.IO;
 using System.Net;
 using System;
 
 // void main()
 // {
-    ServicePointManager.Expect100Continue = true;
-    ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+ServicePointManager.Expect100Continue = true;
+ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
-    var url = "https://github.com/oleg-shilo/GC-Menu/releases/download/v1.0.1/gc-menu.7z";
+var url = "https://github.com/oleg-shilo/GC-Menu/releases/download/v1.0.2/gc-menu.7z";
 
 var installScript = @"tools\chocolateyInstall.ps1";
 
@@ -18,16 +18,16 @@ var cheksum = calcChecksum(url);
 // var cheksum = "E1809AD6433A91B2FF4803E7F4B15AE0FA88905A28949EAC5590F7D9FD9BE9C3";
 Console.WriteLine(cheksum);
 
-    var code = File.ReadAllText(installScript + ".template")
-                   .Replace("$url = ???", "$url = '" + url + "'")
-                   .Replace("$checksum = ???", "$cheksum = '" + cheksum + "'");
+var code = File.ReadAllText(installScript + ".template")
+               .Replace("$url = ???", "$url = '" + url + "'")
+               .Replace("$checksum = ???", "$cheksum = '" + cheksum + "'");
 
 File.WriteAllText(installScript, code);
-    Console.WriteLine("--------------");
-    Console.WriteLine(code);
-    Console.WriteLine("--------------");
-    Console.WriteLine();
-    Console.WriteLine("Done...");
+Console.WriteLine("--------------");
+Console.WriteLine(code);
+Console.WriteLine("--------------");
+Console.WriteLine();
+Console.WriteLine("Done...");
 // }
 
 string calcChecksum(string url)
