1dad9a0f62f0cbde9fe1c2cd19857cde844944ab
Oleg Shilo <oleg.shilo@gmail.com>
2024-12-17 17:35:00 +1100

    published on Chocolatey

--- choco/tools/chocolateyInstall.ps1
+++ choco/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 $packageName = 'GC-Menu'
-$url = 'https://github.com/oleg-shilo/GC-Menu/releases/download/v1.0.2/gc-menu.7z'
+$url = 'https://github.com/oleg-shilo/GC-Menu/releases/download/v1.0.3/gc-menu.7z'
 
 Stop-Process -Name "gc-menu"
 
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$cheksum = '9E686C66E9F4B40CC34315A2DE091F520C55BF7F5822461C11C7B9B35E459F08'
+$cheksum = '44DA8DB0BE2F1ECBA0F41E9E8FA9C68A474B6686CB799BCB2111778338F53DD4'
 $checksumType = "sha256"
 
 # Download and unpack a zip file
